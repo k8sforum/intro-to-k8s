f6a00036c72596961e62161b7eb588fa7f0a0981
Tshepo Ntlhokoa <tshepo.ntlhokoa@gmail.com>
2026-07-31 21:02:24 +0200

    chore: update api version number

--- 0-preview/scripts/merge-manifests.ps1
+++ 0-preview/scripts/merge-manifests.ps1
@@ -8,7 +8,7 @@
 # Keep this list in sync with the image names/versions in ../docker-compose.build.yml
 $images = @(
     "tshepontlhokoa/mytravels-migrations:v1.0.1"
-    "tshepontlhokoa/mytravels-api:v1.0.4"
+    "tshepontlhokoa/mytravels-api:v1.0.5"
     "tshepontlhokoa/mytravels-messaging:v1.0.6"
     "tshepontlhokoa/mytravels-web:v1.0.0"
 )
