41515d9f518c6782bb9e4f10bd42c4e841d550a5
Tshepo Ntlhokoa <tshepo.ntlhokoa@gmail.com>
2026-08-02 10:30:24 +0200

    chore: some refactoring

--- 1-dockerize/scripts/migrations.ps1
+++ 1-dockerize/scripts/migrations.ps1
@@ -1,9 +1,10 @@
-cd '../common/mytravels.migration/'
+cd '../../src/common/mytravels.migration/'
+
+dotnet tool restore
 
 #dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/ database drop --context CoreDbContext -f -v
 # dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/  migrations add Init  --context CoreDbContext
 dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/ migrations add UpdateStoredProcedure --context CoreDbContext
 dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/ migrations add SeedData --context CoreDbContext
 # dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/ database update --context CoreDbContext
-#dotnet tool update --global dotnet-ef
 # dotnet ef --startup-project ../../common/mytravels.migration/ --project ../../common/mytravels.domain/ migrations  script -i  --context CoreDbContext -o C:\development\temp\Migrations\migration.sql
