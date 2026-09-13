84cfbc92825d882e21849b84facb17919992f8c4
tshepontlhokoa <tshepo.ntlhokoa@gmail.com>
2026-09-13 07:40:57 +0200

    chore: remove script

--- src/scripts/grant app service access to the db.sql
+++
@@ -1,7 +0,0 @@
-DROP USER IF EXISTS [sanorth-api-prod-mytravels-001];
-CREATE USER [sanorth-api-prod-mytravels-001] FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER [sanorth-api-prod-mytravels-001];
-ALTER ROLE db_datawriter ADD MEMBER [sanorth-api-prod-mytravels-001];
-ALTER ROLE db_ddladmin ADD MEMBER [sanorth-api-prod-mytravels-001];
-GRANT EXEC TO [sanorth-api-prod-mytravels-001];
-GO
